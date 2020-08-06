74b1ed6255d5347f906120bd23d731c743f2bb89
sakthishanmugam02 <35956665+sakthishanmugam02@users.noreply.github.com>
2020-08-06 19:54:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,14 @@ data "ibm_resource_group" "rg" {
   name = "Default"
 }
 
+data "ibm_is_image" "test_schematics_demo_image" {
+  name = "ibm-ubuntu-18-04-1-minimal-amd64-2"
+}
+
+data "ibm_is_ssh_key" "test_schematics_demo_ssh_key" {
+  name = "sakthi-ssh"
+}
+
 resource "ibm_is_vpc" "test_schematics_demo_vpc" {
   depends_on     = [data.ibm_resource_group.rg]
   name           = "test-schematics-demo-vpc"
@@ -45,4 +53,77 @@ resource "ibm_dns_resource_record" "test_schematics_demo_pdns_record_a" {
   name        = "testA"
   rdata       = "1.2.3.5"
   ttl         = 900
+}
+
+resource "ibm_is_subnet" "test_schematics_demo_subnet" {
+  name            = "test-schematics-demo-subnet"
+  vpc             = ibm_is_vpc.f5_terraform_ansible_vpc.id
+  zone            = var.zone
+  ipv4_cidr_block = "10.240.0.0/24"
+}
+
+resource "ibm_is_security_group" "test_schematics_demo_sg" {
+  name           = "test-schematics-demo-sg"
+  vpc            = ibm_is_vpc.test_schematics_demo_vpc.id
+  resource_group = data.ibm_resource_group.rg.id
+}
+
+resource "ibm_is_security_group_rule" "test_schematics_demo_sg_rule_ssh" {
+  depends_on = [ibm_is_security_group.test_schematics_demo_sg]
+  group      = ibm_is_security_group.test_schematics_demo_sg.id
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+  tcp {
+    port_min = 22
+    port_max = 22
+  }
+}
+
+resource "ibm_is_security_group_rule" "test_schematics_demo_sg_rule_all_in" {
+  depends_on = [ibm_is_security_group_rule.test_schematics_demo_sg_rule_ssh]
+  group      = ibm_is_security_group.test_schematics_demo_sg.id
+  direction  = "inbound"
+  remote     = "0.0.0.0/0"
+}
+
+resource "ibm_is_security_group_rule" "test_schematics_demo_sg_rule_all_out" {
+  depends_on = [ibm_is_security_group_rule.test_schematics_demo_sg_rule_all_in]
+  group      = ibm_is_security_group.test_schematics_demo_sg.id
+  direction  = "outbound"
+  remote     = "0.0.0.0/0"
+}
+
+resource "ibm_is_instance" "test_schematics_demo_vsi" {
+  depends_on = [ibm_is_security_group_rule.test_schematics_demo_sg_rule_all_out]
+  name           = "test-schematics-demo-vsi"
+  image          = data.ibm_is_image.test_schematics_demo_image.id
+  profile        = "bx2-16x64"
+  resource_group = data.ibm_resource_group.rg.id
+
+  primary_network_interface {
+    subnet          = ibm_is_subnet.test_schematics_demo_subnet.id
+    security_groups = [ibm_is_security_group.f5_terraform_ansible_host_vsi_sg.id]
+  }
+
+  vpc  = ibm_is_vpc.test_schematics_demo_vpc.id
+  zone = "us-south-1"
+  keys = ["${data.ibm_is_ssh_key.test_schematics_demo_ssh_key.id}"]
+
+  provisioner "local-exec" {
+    command = "sleep 30"
+  }
+
+  #provisioner "local-exec" {
+  #  command = "ansible-playbook -i '${ibm_is_floating_ip.address},' --private-key ${var.private_key_path} ../ansible/httpd.yml"
+  #}
+
+  timeouts {
+    create = "10m"
+    delete = "10m"
+  }
+}
+
+resource "ibm_is_floating_ip" "test_schematics_demo_fip" {
+  name   = "test-schematics-demo-fip"
+  target = ibm_is_instance.test_schematics_demo_vsi.primary_network_interface.0.id
 }
